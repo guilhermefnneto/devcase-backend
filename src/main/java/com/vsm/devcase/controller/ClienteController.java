5d8cb8a122a98177cd3e8b7eaedde7fcb4710cc9
guilhermefnneto <guilherme.fn.neto@outlook.com>
2019-03-04 17:53:09 -0300

    Inclusão da anotation @CrossOrigin.

--- src/main/java/com/vsm/devcase/controller/ClienteController.java
+++ src/main/java/com/vsm/devcase/controller/ClienteController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
@@ -26,6 +27,7 @@ import com.vsm.devcase.validation.ClienteValidation;
  */
 @Controller
 @RequestMapping("/cliente")
+@CrossOrigin // Solução temporária para atender apenas à fase do desenvolvimento front-end.
 public class ClienteController {
 
 	
--- src/main/java/com/vsm/devcase/controller/PontuacaoController.java
+++ src/main/java/com/vsm/devcase/controller/PontuacaoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
@@ -25,6 +26,7 @@ import com.vsm.devcase.validation.PontuacaoValidation;
  */
 @Controller
 @RequestMapping("/pontuacao")
+@CrossOrigin // Solução temporária para atender apenas à fase do desenvolvimento front-end.
 public class PontuacaoController {
 
 	
